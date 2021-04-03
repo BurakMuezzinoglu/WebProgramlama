addfb155a3b9e2363a58bb47695045f8f4d6f64c
BurakMuezzinoglu <burakmuzzy@gmail.com>
2021-04-04 02:40:37 +0300

    Websitesi için ilk dökümantasyon

---
+++ Proje.Site/Default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Proje.Site
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            LabelSlider1.Text = "Explore";
+            LabelDesc1.Text = "Explore dozens of creative image and stock footages or give people clue about your passionate!";
+            LabelSlider2.Text = "Inspire";
+            LabelDesc2.Text ="Take inspiration from choosen images or help community to take their roadmap for future works or projects!";
+            LabelSlider3.Text = "Share";
+            LabelDesc3.Text ="Share your example work/works with footage deposit to contribute enlargement of image world!";
+
+        }
+    }
+}
---
+++ Proje.Site/Test.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/AnaSayfa.Master" AutoEventWireup="true" CodeBehind="Test.aspx.cs" Inherits="Proje.Site.Test" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <p>
+        vcvdfdfdfdfsfsdfsdfsdfsd</p>
+</asp:Content>
