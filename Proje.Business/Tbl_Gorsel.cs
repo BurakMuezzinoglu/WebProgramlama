02e201a352776d3cc7fb827bd4252f7c3eae8b48
BurakMuezzinoglu <burakmuzzy@gmail.com>
2021-04-21 22:50:59 +0300

    veritabanı classları oluşturma

---
+++ Proje.Business/Tbl_Gorsel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proje.Business
+{
+    public class Tbl_Gorsel
+    {
+        public string Gorselid { get; set; }
+        public string GorselAd { get; set; }
+        public string Gorselİcerik { get; set; }
+        public string GorselKategori { get; set; }
+        public string GorselAciklama { get; set; }
+        public string GorselEklenmeTarih { get; set; }
+
+
+
+    }
+}
